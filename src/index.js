69897ee286a1e0ddfdb095a71cf7d2205126b966
Tri Tran <tranminhtri9090@gmail.com>
2021-04-26 14:14:28 +1000

    Initialize Amplify

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Amplify from 'aws-amplify';
+import aws_exports from './aws-exports';
+
+Amplify.configure(aws_exports);
 
 ReactDOM.render(
   <React.StrictMode>

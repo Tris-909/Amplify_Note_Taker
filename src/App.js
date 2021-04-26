c1970e0ebeaf15c0a296dfdbeefa08dd4c434b5b
Tri Tran <tranminhtri9090@gmail.com>
2021-04-26 15:27:52 +1000

    Adding Authentication to the app using Amplify

--- src/App.js
+++ src/App.js
@@ -1,9 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { withAuthenticator, AmplifySignOut  } from '@aws-amplify/ui-react';
 
 const App = () => {
+  const [notes, setNotes] = useState([{
+    id: 1,
+    note: 'Test'
+  }]);
+
+  const onSubmitHandler = () => {
+
+  }
+
   return (
-    <div>App</div>
+    <>
+      <AmplifySignOut />
+      <div 
+        className="
+          flex 
+          flex-column 
+          items-center 
+          justify-center 
+          pa3 
+          bg-washed-red
+        ">
+          <h1 className="code f2-1">
+            Amplify Note Taker
+          </h1>
+          <form className="mb3" onSubmit={onSubmitHandler}>
+            <input type="text" className="pa2 f4" placeholder="Write a note" /> 
+            <button type="submit">Add Note</button>
+          </form>
+          <div>
+            {notes.map((note) => {
+              return(
+                <div className="flex items-center" key={note.id}>
+                  <li className="list pa3 f3">
+                    { note.note }
+                    <button className="bg-transparent bn f4">
+                      <span>&times;</span>
+                    </button>
+                  </li> 
+                </div>
+              )
+            })}
+          </div>
+      </div>
+    </>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
